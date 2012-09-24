65dde8f6bcb8254a6db6a597802ababe4a9d625d
Kolia Morev <kolyuchiy@gmail.com>
2012-09-24 15:36:22 +0400

    Add initWithCoder method for FoldView

--- PaperFold/PaperFold/PaperFold/FoldView.m
+++ PaperFold/PaperFold/PaperFold/FoldView.m
@@ -42,49 +42,62 @@
 @synthesize state = _state;
 @synthesize contentView = _contentView;
 
+- (void)setupWithFrame:(CGRect)frame
+{
+    
+    
+    
+    // foldview consists of leftView and rightView, and a content view
+    // set shadow direction of leftView and rightView such that the shadow falls on the fold in the middle
+    
+    // content view holds a subview which is the actual displayed content
+    // contentView is required as a wrapper of the original content because it is better to take a screenshot of the wrapper view layer
+    // taking a screenshot of a tableview layer directly for example, may end up with blank view because of recycled cells
+    _contentView = [[UIView alloc] initWithFrame:CGRectMake(0,0,frame.size.width,frame.size.height)];
+    [_contentView setBackgroundColor:[UIColor clearColor]];
+    [self addSubview:_contentView];
+    
+    // set anchor point of the leftView to the left edge
+    _leftView = [[FacingView alloc] initWithFrame:CGRectMake(-1*frame.size.width/4,0,frame.size.width/2,frame.size.height)];
+    [_leftView setBackgroundColor:[UIColor colorWithWhite:0.99 alpha:1]];
+    [_leftView.layer setAnchorPoint:CGPointMake(0.0, 0.5)];
+    [self addSubview:_leftView];
+    [_leftView.shadowView setColorArrays:[NSArray arrayWithObjects:[UIColor colorWithWhite:0 alpha:0.05],[UIColor colorWithWhite:0 alpha:0.6], nil]];
+    
+    // set anchor point of the rightView to the right edge
+    _rightView = [[FacingView alloc] initWithFrame:CGRectMake(-1*frame.size.width/4,0,frame.size.width/2,frame.size.height)];
+    [_rightView setBackgroundColor:[UIColor colorWithWhite:0.99 alpha:1]];
+    [_rightView.layer setAnchorPoint:CGPointMake(1.0, 0.5)];
+    [self addSubview:_rightView];
+    [_rightView.shadowView setColorArrays:[NSArray arrayWithObjects:[UIColor colorWithWhite:0 alpha:0.9],[UIColor colorWithWhite:0 alpha:0.55], nil]];
+    
+    // set perspective of the transformation
+    CATransform3D transform = CATransform3DIdentity;
+    transform.m34 = -1/500.0;
+    [self.layer setSublayerTransform:transform];
+    
+    // make sure the views are closed properly when initialized
+    [_leftView.layer setTransform:CATransform3DMakeRotation((M_PI / 2), 0, 1, 0)];
+    [_rightView.layer setTransform:CATransform3DMakeRotation((M_PI / 2), 0, 1, 0)];
+    
+    [self setAutoresizesSubviews:YES];
+    [_contentView setAutoresizesSubviews:YES];
+    [_contentView setAutoresizingMask:UIViewAutoresizingFlexibleHeight];
+}
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
-        
-        
-        // foldview consists of leftView and rightView, and a content view
-        // set shadow direction of leftView and rightView such that the shadow falls on the fold in the middle
-        
-        // content view holds a subview which is the actual displayed content
-        // contentView is required as a wrapper of the original content because it is better to take a screenshot of the wrapper view layer
-        // taking a screenshot of a tableview layer directly for example, may end up with blank view because of recycled cells
-        _contentView = [[UIView alloc] initWithFrame:CGRectMake(0,0,frame.size.width,frame.size.height)];
-        [_contentView setBackgroundColor:[UIColor clearColor]];
-        [self addSubview:_contentView];
-        
-        // set anchor point of the leftView to the left edge
-        _leftView = [[FacingView alloc] initWithFrame:CGRectMake(-1*frame.size.width/4,0,frame.size.width/2,frame.size.height)];
-        [_leftView setBackgroundColor:[UIColor colorWithWhite:0.99 alpha:1]];
-        [_leftView.layer setAnchorPoint:CGPointMake(0.0, 0.5)];
-        [self addSubview:_leftView];
-        [_leftView.shadowView setColorArrays:[NSArray arrayWithObjects:[UIColor colorWithWhite:0 alpha:0.05],[UIColor colorWithWhite:0 alpha:0.6], nil]];
-        
-        // set anchor point of the rightView to the right edge
-        _rightView = [[FacingView alloc] initWithFrame:CGRectMake(-1*frame.size.width/4,0,frame.size.width/2,frame.size.height)];
-        [_rightView setBackgroundColor:[UIColor colorWithWhite:0.99 alpha:1]];
-        [_rightView.layer setAnchorPoint:CGPointMake(1.0, 0.5)];
-        [self addSubview:_rightView];
-        [_rightView.shadowView setColorArrays:[NSArray arrayWithObjects:[UIColor colorWithWhite:0 alpha:0.9],[UIColor colorWithWhite:0 alpha:0.55], nil]];
-        
-        // set perspective of the transformation
-        CATransform3D transform = CATransform3DIdentity;
-        transform.m34 = -1/500.0;
-        [self.layer setSublayerTransform:transform];
-        
-        // make sure the views are closed properly when initialized
-        [_leftView.layer setTransform:CATransform3DMakeRotation((M_PI / 2), 0, 1, 0)];
-        [_rightView.layer setTransform:CATransform3DMakeRotation((M_PI / 2), 0, 1, 0)];
-        
-        [self setAutoresizesSubviews:YES];
-        [_contentView setAutoresizesSubviews:YES];
-        [_contentView setAutoresizingMask:UIViewAutoresizingFlexibleHeight];
+        [self setupWithFrame:frame];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self setupWithFrame:self.frame];
     }
     return self;
 }
